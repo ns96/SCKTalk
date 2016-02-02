8b4c1fea551691262f296094031e5caea64916c4
Nathan <ns96@nyu.edu>
2016-02-01 19:36:12 -0500

    Version 1.1 with stage simulator

--- src/org/instras/sck/DipCoaterSimulator.java
+++ src/org/instras/sck/DipCoaterSimulator.java
@@ -27,15 +27,13 @@ public class DipCoaterSimulator extends JDialog {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Nathan Stevens
+        // Generated using JFormDesigner non-commercial license
         dialogPane = new JPanel();
         contentPanel = new JPanel();
         stageSlider = new JSlider();
         buttonBar = new JPanel();
-        okButton = new JButton();
 
         //======== this ========
-        setTitle("Dip Coater Simulator");
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
@@ -43,14 +41,6 @@ public class DipCoaterSimulator extends JDialog {
         //======== dialogPane ========
         {
             dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
-
-            // JFormDesigner evaluation mark
-            dialogPane.setBorder(new javax.swing.border.CompoundBorder(
-                new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                    "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                    javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                    java.awt.Color.red), dialogPane.getBorder())); dialogPane.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
             dialogPane.setLayout(new BorderLayout());
 
             //======== contentPanel ========
@@ -62,9 +52,9 @@ public class DipCoaterSimulator extends JDialog {
                 stageSlider.setMajorTickSpacing(10);
                 stageSlider.setMinorTickSpacing(1);
                 stageSlider.setPaintTicks(true);
-                stageSlider.setPaintLabels(true);
                 stageSlider.setValue(90);
                 stageSlider.setEnabled(false);
+                stageSlider.setPaintLabels(true);
                 contentPanel.add(stageSlider, BorderLayout.CENTER);
             }
             dialogPane.add(contentPanel, BorderLayout.CENTER);
@@ -75,12 +65,6 @@ public class DipCoaterSimulator extends JDialog {
                 buttonBar.setLayout(new GridBagLayout());
                 ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 80};
                 ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0};
-
-                //---- okButton ----
-                okButton.setText("OK");
-                buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
-                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                    new Insets(0, 0, 0, 0), 0, 0));
             }
             dialogPane.add(buttonBar, BorderLayout.SOUTH);
         }
@@ -91,11 +75,10 @@ public class DipCoaterSimulator extends JDialog {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Nathan Stevens
+    // Generated using JFormDesigner non-commercial license
     private JPanel dialogPane;
     private JPanel contentPanel;
     private JSlider stageSlider;
     private JPanel buttonBar;
-    private JButton okButton;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
